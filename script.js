69661b32b50247f559a1c96a465772ac3c7471ac
ashifayusaf <105366441+ashifayusaf@users.noreply.github.com>
2022-05-12 18:20:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function clearScreen() {
     document.getElementById("result").value = "";
    }
    
-   // value display
+
    function display(value) {
     document.getElementById("result").value += value;
    }
-   // This function evaluates the expression and return result
+
    function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
